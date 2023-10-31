c03d31802addc153036606cd881f7d77b1b80510
Hadassah Uche <uhadassah1@gmail.com>
2023-10-31 12:28:27 +0200

    Added the code for the enter book details function and called it in the main

--- UcheHadassah7_2/HadassahUche7_2.cpp
+++ UcheHadassah7_2/HadassahUche7_2.cpp
@@ -30,13 +30,107 @@ void displayBookDetail(const BookData&);//I dont get why this is passed as a con
 int CreateISBN();
 int main()
 {
-	BookData Book = {"Forced Love, Forged Hearts","Uche Hadassah","Emyrixx",30,5,2023,"EM0001",25.00};//This is probably wrong. I'll figure it out later
-	displayBookDetail(Book);
+	BookData Book[100];// Array stores 100 books
+	int bookCount = 0;
+	int Option;
+	do
+
+	{
+		cout << "Menu" << endl;
+		cout << "1) Enter new book" << endl;
+		cout << "2) Display all books" << endl;
+		cout << "3) Display book by index" << endl;
+		cout << "4) Display book by Author" << endl;
+		cout << "5) Display book by Title" << endl;
+		cout << "6) Display  by publisher" << endl;
+		cout << "7) Exit" << endl;
+		cout << " Enter your choice:";
+		cin >> Option;
+		cin.ignore();
+		if (Option == 1)
+		{
+			if (bookCount < 100)
+			{
+				enterBookDetail(Book[bookCount]);
+				bookCount++;
+			}
+			else
+			{
+				cerr << "No more room to store more books" << endl;
+			}
+		}
+		else if (Option == 2)
+		{
+			for (int i = 0; i < bookCount; i++)
+			{
+				displayBookDetail(Book[i]);
+			}
+		}
+	} while (Option < 1);
+	
 	return 0;
 }
 void enterBookDetail(BookData& Book)
 {
-	
+	cout << "Enter Title:";
+	cin.getline(Book.title, 50);
+	cout << "Enter Author:";
+	cin.getline(Book.author, 50);
+	cout << "Enter Publisher:";
+	cin.getline(Book.publisher, 50);
+	cout << "Enter date of publication.";
+	//Have to verify the user input. NEVER TRUST THE USER
+	do
+	{
+		cout << "\nYear:";
+		cin >> Book.publication.year;
+		if (Book.publication.year % 4 != 0)
+		{
+			do
+			{
+				cout << "\nMonth:";
+				cin >> Book.publication.month;
+				if (Book.publication.month == 2)
+				{
+					do
+					{
+						cout << "\nDay:";
+						cin >> Book.publication.day;
+					} while (Book.publication.day < 0 && Book.publication.day > 28);
+				}
+				else if (Book.publication.month == 4 || Book.publication.month == 6 || Book.publication.month == 9 || Book.publication.month == 11)
+				{
+					do
+					{
+						cout << "\nDay:";
+						cin >> Book.publication.day;
+					} while (Book.publication.day < 0 && Book.publication.day > 30);
+				}
+				else
+				{
+					do
+					{
+						cout << "\nDay:";
+						cin >> Book.publication.day;
+					} while (Book.publication.day < 0 && Book.publication.day > 31);
+				}
+			} while (Book.publication.month < 0 && Book.publication.month > 12);//Verify that the month is within bounds
+		}
+
+	} while (Book.publication.year < 1 && Book.publication.year > 2023);//Verify that the date is within bounds
+	// Get the price from the user
+	do
+	{
+		cout << "Enter the price of the book:";
+		cin >> Book.price;
+	} while (Book.price < 0.0);// verify that the price is not less than 0
+}
+int CreateISBN()
+{
+	static int count = 001;/*A static variable does not reset when the function is re - called.
+	ideal for generation unique variables
+	*/
+	return count++;
 }
 void displayBookDetail(const BookData& Book)
 {
@@ -45,10 +139,4 @@ void displayBookDetail(const BookData& Book)
 	cout << "ISBN:" << Book.ISBN << endl;
 	cout << "Price:" << Book.price << endl;
 }
-int CreateISBN()
-{
-	static int count = 0;/*A static variable does not reset when the function is re - called.
-	ideal for generation unique variables
-	*/
-	return count++;
-}
+
