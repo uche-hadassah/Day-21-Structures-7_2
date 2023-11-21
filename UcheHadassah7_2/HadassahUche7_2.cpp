4af8d6806cb67178dd424e8dab62f16a46d00b7f
Hadassah Uche <uhadassah1@gmail.com>
2023-11-21 14:01:26 +0200

    Implemented the code for option 1

--- UcheHadassah7_2/HadassahUche7_2.cpp
+++ UcheHadassah7_2/HadassahUche7_2.cpp
@@ -8,10 +8,43 @@ book from the user and then displays the information entered.*/
 using namespace std;
 struct Book
 {
-	char author[100];
 	char title[100];
+	char author[100];
 	char publisher[100];
 	int yearOfPublication;
 	int isbn = 0;
 	double price;
 };
+int main()
+{
+	Book bookDetails;
+	int option;
+	for(int i = 0;i<100;i++)
+	{
+		cout << "\nMENU";
+		cout << "\n1) Enter new book";
+		cout << "\n2) Display all books";
+		cout << "\n3) Exit";
+		cout << "\n Enter an option:";
+		cin >> option;
+		if(option == 1)
+		{
+			cout << "Enter the details of the book.";
+			cout << "\nTitle:";
+			cin.getline(bookDetails.title, 100);
+			cout << "\nAuthor:";
+			cin.getline(bookDetails.author, 100);
+			cout << "Publisher:";
+			cin.getline(bookDetails.publisher, 100);
+			cout << "\nYear of publication:";
+			cin >> bookDetails.yearOfPublication;
+			cout << "\nPrice:";
+			cin >> bookDetails.price;
+		}
+		else if (option == 2)
+		{
+
+		}
+	}
+
+}
