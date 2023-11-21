89ad9f216913662c72c465bb1c1d985a07b72a6b
Hadassah Uche <uhadassah1@gmail.com>
2023-11-21 13:46:14 +0200

    defined the Book structure datatype

--- UcheHadassah7_2/HadassahUche7_2.cpp
+++ UcheHadassah7_2/HadassahUche7_2.cpp
@@ -3,3 +3,15 @@ books on sale. The details should include: author, title, publisher, year of pub
 ISBN and price. 
 b) Write a program that creates a Book structure variable and accepts the data of a 
 book from the user and then displays the information entered.*/
+#include<iostream>
+#include<cstring>
+using namespace std;
+struct Book
+{
+	char author[100];
+	char title[100];
+	char publisher[100];
+	int yearOfPublication;
+	int isbn = 0;
+	double price;
+};
