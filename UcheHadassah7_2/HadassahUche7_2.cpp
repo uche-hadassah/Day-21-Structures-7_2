3b7a1b4f5c70d4890d17ec9d6b72c5f2a7c77248
Hadassah Uche <uhadassah1@gmail.com>
2023-11-21 14:32:18 +0200

    I'm trying not to confuse myself

--- UcheHadassah7_2/HadassahUche7_2.cpp
+++ UcheHadassah7_2/HadassahUche7_2.cpp
@@ -6,45 +6,72 @@ book from the user and then displays the information entered.*/
 #include<iostream>
 #include<cstring>
 using namespace std;
+const int MAX = 100;
 struct Book
 {
 	char title[100];
 	char author[100];
 	char publisher[100];
 	int yearOfPublication;
-	int isbn = 0;
+	int isbn;
 	double price;
 };
 int main()
 {
-	Book bookDetails;
+	Book bookDetails[100];
+	int i = 0;
 	int option;
-	for(int i = 0;i<100;i++)
+	do
 	{
-		cout << "\nMENU";
+		cout << "MENU";
 		cout << "\n1) Enter new book";
 		cout << "\n2) Display all books";
 		cout << "\n3) Exit";
 		cout << "\n Enter an option:";
 		cin >> option;
+		while(option < 1 || option > 3)
+		{
+			cout << "Invalid. Please enter an option:";
+			cin >> option;
+		}
+		cin.ignore();
 		if(option == 1)
 		{
 			cout << "Enter the details of the book.";
 			cout << "\nTitle:";
-			cin.getline(bookDetails.title, 100);
+			cin.getline(bookDetails[i].title, 100);
 			cout << "\nAuthor:";
-			cin.getline(bookDetails.author, 100);
-			cout << "Publisher:";
-			cin.getline(bookDetails.publisher, 100);
+			cin.getline(bookDetails[i].author, 100);
+			cout << "\nPublisher:";
+			cin.getline(bookDetails[i].publisher, 100);
 			cout << "\nYear of publication:";
-			cin >> bookDetails.yearOfPublication;
+			cin >> bookDetails[i].yearOfPublication;
 			cout << "\nPrice:";
-			cin >> bookDetails.price;
+			cin >> bookDetails[i].price;
+			i++;
 		}
 		else if (option == 2)
 		{
+			int j = 0;
+			bookDetails[j].isbn = 0;
+			for(;j<i;j++,bookDetails[j].isbn++)
+			{
 
+				cout << "\nTitle:" << bookDetails[j].title;
+				cout << "\nAuthor:" << bookDetails[j].author;
+				cout << "\nPublisher:" << bookDetails[j].publisher;
+				cout << "\nYear of publication:" << bookDetails[j].yearOfPublication;
+				cout << "\nISBN:EMYR" << bookDetails[j].isbn;
+				cout << "\nPrice:" << bookDetails[j].price;
+				
+			}
+			cout << endl;
 		}
-	}
+		else
+		{
+			cout << "Thank you for your time^^";
+		}
+		
+	} while (option != 3 && i < 100);
 
 }
